9e3618ed676308c87e630a265198f0824dd9fe75
EngineeredReiwa <k.keisuke.1230@gmail.com>
2021-08-21 07:27:05 +0000

    first commit

---
+++ mentor.rb
@@ -0,0 +1,30 @@
+class Mentor 
+    attr_accessor :name 
+    
+    def initialize(name)
+        self.name = name
+    end
+    
+    def job
+        puts "#{self.name}です。私は現役のITプロフェッショナルです。"
+    end
+    
+end
+
+class RailsMentor < Mentor
+    
+    def initialize(name)
+        super(name)
+    end
+    
+    def job
+        puts "#{self.name}です。私はRubyとRailsでWebアプリケーションを作ります。"
+    end
+end
+
+
+mentor = Mentor.new("煌木")
+railsMentor = RailsMentor.new('赤出')
+
+mentor.job()
+railsMentor.job()
